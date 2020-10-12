60bbd272f1c32c094a0a7b4c89b08f2d30c4cbd6
eklez <mochaccino.00.00@gmail.com>
2020-10-12 01:59:45 -0700

    Redefine the map!

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from block import Block
 
 def main_start ():
     mainMap = MainMap ()
-    mainMap.initialize (4720)
+    mainMap.initialize (8020)
     new_block = Block (0, 4)
     ret = mainMap.addblock (10,10, new_block)
+
     for i in ret:
-        mainMap.updatemap (i)
-        mainMap.printmap (1)
+        i.printmap (1)
         print ("")
 
 
--- map.py
+++ map.py
@@ -32,19 +32,20 @@ class MainMap:
             for j in range (5):
                 if block.map[i,j] == 0:
                     continue
-                new_map = copy.deepcopy (self.mapArray[:,:,:])
-                new_map_c = new_map[row-j:row-j+5, column-i:column-i+5,1]
-                new_map_c -= block.map
+                new_obj = copy.deepcopy (self)
+                new_map = new_obj.mapArray[:,:,:]
+                new_map = new_map[row-j:row-j+5, column-i:column-i+5,1]
+                new_map -= block.map
 
                 # Check possibility
                 possi = True
                 for ii in range (5):
                     for jj in range (5):
-                        if new_map_c[ii,jj] < 0:
+                        if new_map[ii,jj] < 0:
                             possi = False
 
                 if possi == True:
-                    retq.append (new_map)
+                    retq.append (new_obj)
 
         return retq
 
@@ -55,14 +56,16 @@ class MainMap:
         emptyline = int ((MainMap.MAX_WIDTH - self.curWidth)/2) + 2
 
         print ("    ",end='')
-        for i in range (emptyline, MainMap.MAX_WIDTH-emptyline):
+        for i in range (emptyline, MainMap.MAX_WIDTH+4-emptyline):
             print ("%02d  "%(i), end='')
         print ("")
         print ("    " + "----"*24)
-        for i in range (emptyline, MainMap.MAX_HEIGHT-emptyline):
+        for i in range (emptyline, MainMap.MAX_HEIGHT+4-emptyline):
             print ("%02d| "%(i),end='')
             for j in range (emptyline, MainMap.MAX_WIDTH-emptyline):
+                print ("\033[0;%dm" %(30+self.mapArray[i,j,0]%8), end='')
                 print ("%02d  " %(self.mapArray[i,j,e]), end='')
+                print ("\033[0m", end='')
             print ("")
 
     def initialize(self, union):
@@ -106,42 +109,42 @@ class MainMap:
 
         # Initialize Map
         emptyline = int ((MainMap.MAX_WIDTH - self.curWidth)/2) + 2
-        for i in range (emptyline, MainMap.MAX_HEIGHT-emptyline):
-            for j in range (emptyline, MainMap.MAX_WIDTH-emptyline):
+        for i in range (emptyline, MainMap.MAX_HEIGHT+4-emptyline):
+            for j in range (emptyline, MainMap.MAX_WIDTH+4-emptyline):
                 # Set current slot is possible
                 self.mapArray[i,j,1] = 1
                 # Set section of map
-                if i >= j and i < (MainMap.MAX_WIDTH-j) and i < 11 and j < 11:
+                if i >= j and i < (MainMap.MAX_WIDTH+2-j) and i < 13 and j < 13:
                     self.mapArray[i,j,0] = 0
-                    if j > 5:
+                    if j > 7:
                         self.mapArray[i,j,0] += 1
-                elif i >= j and i < (MainMap.MAX_WIDTH-j) and i >= 11 and j < 11:
+                elif i >= j and i < (MainMap.MAX_WIDTH+2-j) and i >= 13 and j < 13:
                     self.mapArray[i,j,0] = 2
-                    if j > 5:
+                    if j > 7:
                         self.mapArray[i,j,0] += 1
-                elif i >= j and i >= (MainMap.MAX_WIDTH-j) and i >= 11 and j < 11:
+                elif i >= j and i >= (MainMap.MAX_WIDTH+2-j) and i >= 13 and j < 13:
                     self.mapArray[i,j,0] = 4
                     if i < 17:
                         self.mapArray[i,j,0] += 1
-                elif i >= j and i >= (MainMap.MAX_WIDTH-j) and i >= 11 and j >= 11:
+                elif i >= j and i >= (MainMap.MAX_WIDTH+2-j) and i >= 13 and j >= 13:
                     self.mapArray[i,j,0] = 6
                     if i < 17:
                         self.mapArray[i,j,0] += 1
-                elif i < j and i >= (MainMap.MAX_WIDTH-j) and i >= 11 and j >= 11:
+                elif i < j and i >= (MainMap.MAX_WIDTH+2-j) and i >= 13 and j >= 13:
                     self.mapArray[i,j,0] = 8
                     if j < 17:
                         self.mapArray[i,j,0] += 1
-                elif i < j and i >= (MainMap.MAX_WIDTH-j) and i < 11 and j >= 11:
+                elif i < j and i >= (MainMap.MAX_WIDTH+2-j) and i < 13 and j >= 13:
                     self.mapArray[i,j,0] = 10
                     if j < 17:
                         self.mapArray[i,j,0] += 1
-                elif i < j and i < (MainMap.MAX_WIDTH-j) and i < 11 and j >= 11:
+                elif i < j and i < (MainMap.MAX_WIDTH+2-j) and i < 13 and j >= 13:
                     self.mapArray[i,j,0] = 12
-                    if i > 5:
+                    if i > 7:
                         self.mapArray[i,j,0] += 1
-                elif i < j and i < (MainMap.MAX_WIDTH-j) and i < 11 and j < 11:
+                elif i < j and i < (MainMap.MAX_WIDTH+2-j) and i < 13 and j < 13:
                     self.mapArray[i,j,0] = 14
-                    if i > 5:
+                    if i > 7:
                         self.mapArray[i,j,0] += 1
                 else:
                     print ("Couldn't be here i : %d j : %d" %(i,j))
